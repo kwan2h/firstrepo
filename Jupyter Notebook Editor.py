6056beed244890d7b376a58c5bfdc41892e7b9f4
kwan2h <58477684+kwan2h@users.noreply.github.com>
2023-08-01 21:52:00 -0700

    Add files via upload

---
+++ Jupyter Notebook Editor.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# <b> Objectives: </b>
+# - List popular languages for Data Science
+# - List common libraries for Data Science
+# - Perform basic computation using Jupyter Notebook
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. JS
+# 4. Ruby
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1.  Pandas
+# 2. Sk-Learn
+# 3. TensorFlow
+
+# | Data Science Tools |
+# | --- |
+# |JupyterLab|
+# |Spyder|
+# |RStudio|
+# |Apache Spark|
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[3]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[4]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+200/60
+
+
+# ## Author
+# Hoi Kwan
